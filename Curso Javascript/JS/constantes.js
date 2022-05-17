fef493fcabdaffb9c1ffc819d9f41eb7e1079dd5
victor garcia <moyosxtouch@gmail.com>
2022-05-16 21:49:55 -0500

    commit

---
+++ Curso Javascript/JS/constantes.js
@@ -0,0 +1,12 @@
+export const PI = Math.PI;
+export let usuario = "Moyo";
+export let password = "qwerty";
+
+export function saludar() {
+  console.log("Hola modulos +ES6");
+}
+export default class hi {
+  constructor() {
+    console.log("Hola clases +ES6");
+  }
+}
---
+++ Curso Javascript/JS/modulos.js
@@ -0,0 +1,9 @@
+import hi, { PI, usuario } from "./constantes.js";
+import { aritmetica as ari } from "./aritmetica.js";
+console.log("Archivo modulos JS");
+console.log(PI, usuario);
+console.log(ari.sumar(5, 5));
+console.log(ari.restar(5, 2));
+
+let hola = new hi();
+hola;
